da45a1b2a1962b54be5db1b48826f71fcfe8d938
Pavlos Nicolaou <vl1s@Pavloss-MacBook-Pro.local>
2016-08-30 18:47:14 +0300

    fixed the issue with camera record

---
+++ SnapDevChat/APPLCameraVCDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  APPLCameraVCDelegate.swift
+//  SnapDevChat
+//
+//  Created by Pavlos Nicolaou on 30/08/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+import Foundation
+
+protocol APPLCameraVCDelegate {
+    
+    func shouldEnabledControls(enabled: Bool)
+    
+}
